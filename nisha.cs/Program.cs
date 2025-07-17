f26db4284f464b8d69d2ad5e1fc15e2a8b9aa3fa
imnishalmc <nishalamichhane254@gmail.com>
2025-07-17 10:44:29 +0545

    first program

---
+++ nisha.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class jagged
+{
+    public static void Main()
+    {
+        int[][] nishalmc = new int[3][] ;
+        nishalmc[0] = new int[3] { 11, 12, 34 };
+        nishalmc[1] = new int[1] { 90 };
+        nishalmc[2] = new int[2] { 35, 67 };
+        for(int i =0;i<3;i++)
+        {
+            Console.WriteLine("jagged array");
+            for(int j = 0; j < nishalmc[i].Length;j++)
+            {
+                Console.WriteLine(nishalmc[i][j] + "");
+            }
+       
+        }
+    }
+}
